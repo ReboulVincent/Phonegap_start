ae53be50368803a25181a7986f4e400625f868c5
Reboul Vincent <reboulvincentjd@gmail.com>
2013-02-13 01:50:58 +0100

    Je comprend rien
    Mais rien du tous

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -1,35 +1,59 @@
 var app = {
 
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-    
+   /*
     showAlert: function (message, title) {//Alert
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
         } else {
             alert(title ? (title + ": " + message) : message);
         }
-    },
-    
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-    
-    initialize: function() {
+    },*/
+    registerEvents: function() {
         var self = this;
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        $(window).on('hashchange', $.proxy(this.route, this));
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
+    },
+    
+        route: function() {//Routeur
+            var hash = window.location.hash;
+            if (!hash) {
+                $('body').html(new HomeView(this.store).render().el);
+                return;
+            }
+            var match = hash.match(app.detailsURL);
+            if (match) {
+                this.store.findById(Number(match[1]), function(employee) {
+                    $('body').html(new EmployeeView(employee).render().el);
+                });
+            }
+        },
+        
+    initialize: function() {//Fonction lancer
+        var self = this;
+        this.detailsURL = /^#employees\/(\d{1,})/;
+        this.registerEvents();
         this.store = new MemoryStore(function() {
-            self.renderHomeView();
+            self.route();
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
-
+    
 };
 
 app.initialize();
